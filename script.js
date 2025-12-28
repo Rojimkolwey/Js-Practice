62adce73e262ce28df325160cf993d63b3195fd5
rojimkolwey <rodgersmkolwe@gmail.com>
2025-12-28 16:30:28 +0300

    parameters

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+// const button = document.getElementById('multiplyBtn');
+
+
+// function multiplyNumber(){
+
+//     let x = Number.document.getElementById('firstNumber').value;
+//     let y = Number.document.getElementById('secondNumber').value;
+
+//     let z = (x*y)
+    
+//     document.getElementById('result').innerHTML=z;
+
+// };
+
+//     button.addEventListener('click', multiplyNumber);
+
+
 const button = document.getElementById('multiplyBtn');
 
+function multiplyNumber(num1,num2){
 
-function multiplyNumber(){
-
-    let x = document.getElementById('firstNumber').value;
-    let y = document.getElementById('secondNumber').value;
-
-    let z = (x*y)
-    
-    document.getElementById('result').innerHTML=z;
-
+    return num1*num2; 
 };
 
-    button.addEventListener('click', multiplyNumber);
+button.addEventListener('click', function(){
+
+    let x = Number(document.getElementById('firstNumber').value);
+    let y = Number(document.getElementById('secondNumber').value);
+
+    let result= multiplyNumber(x, y)
+
+    document.getElementById('result').innerHTML=result;
+});
