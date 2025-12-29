355d23c6e306660a01a192390a4944ac12277527
rojimkolwey <rodgersmkolwe@gmail.com>
2025-12-29 15:59:06 +0300

    Students scores array

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-// const button = document.getElementById('multiplyBtn');
+const scoreText = document.getElementById('scoresDisplay').textContent;
 
+const score = JSON.parse(scoreText);
 
-// function multiplyNumber(){
-
-//     let x = Number.document.getElementById('firstNumber').value;
-//     let y = Number.document.getElementById('secondNumber').value;
-
-//     let z = (x*y)
-    
-//     document.getElementById('result').innerHTML=z;
-
-// };
-
-//     button.addEventListener('click', multiplyNumber);
-
-
-const button = document.getElementById('multiplyBtn');
-
-function multiplyNumber(num1,num2){
-
-    return num1*num2; 
-};
-
-button.addEventListener('click', function(){
-
-    let x = Number(document.getElementById('firstNumber').value);
-    let y = Number(document.getElementById('secondNumber').value);
-
-    let result= multiplyNumber(x, y)
-
-    document.getElementById('result').innerHTML=result;
-});
+console.log(score[0]);
