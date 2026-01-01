7fe469b766a28a3fbfd60071011d7a30f6a73a5b
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-01 14:48:02 +0300

    Array storage

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 
-const button = document.getElementById('checkBtn');
+// const button = document.getElementById('checkBtn');
+
+// button.addEventListener('click', function () {
+//   try {
+//     const ageInput = document.getElementById('ageInput').value;
+
+//     if (ageInput === "") {
+//       throw "Age cannot be empty";
+//     }
+
+//     const age = Number(ageInput);
+
+//     if (isNaN(age)) {
+//       throw "Age must be a number";
+//     }
+
+//     if (age >= 18) {
+//       resultText.innerHTML = "You are old enough";
+//     } else {
+//       resultText.innerHTML = "Sorry, you are too young";
+//     }
+
+//   } catch (error) {
+//     resultText.innerHTML = error;
+//   }
+// });
+
+// try {
+//   // risky code
+// } catch (error) {
+//   console.log(error);
+// } finally {
+//   console.log("Check completed");
+// }
 
 
 
-button.addEventListener('click', function(){
+const fruits =["mango", "eggs"];
 
-    const ageInput = document.getElementById('ageInput').value;
-
-if (ageInput >= 18) {
-    alert('User is old Enough');
-    
-} else{
-    alert('Sorry too young');
-}
-document.getElementById('resultText').innerHTML="Your age is " + ageInput;
-});
-
+ console.log(fruits[0]);
