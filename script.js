24e12e2e7e3fb050f591587d759a074c86afeac5
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-06 17:17:55 +0300

    orm validation

--- script.js
+++ script.js
@@ -1,129 +1,22 @@
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const results = document.getElementById('result');
-
-const addButton = document.getElementById('add');
-const subtractButton = document.getElementById('subtract');
-const multiplyButton = document.getElementById('multiply');
-const divideButton = document.getElementById('divide');
-const equalsButton = document.getElementById('equals');
-const clearButton = document.getElementById('clear');
-
-let currentOperation = '';
-let result="";
-
-addButton.addEventListener('click', function (){
-
-    currentOperation = '+';
-    console.log ('the current opartion id assigned to' , currentOperation);
-});
+const form = document.querySelector('.signup-form');
+const fullName = document.getElementById('fullname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword= document.getElementById('confirmPassword');
+const checkBox = document.getElementById('terms');
+const btn = document.getElementById('createAccount');
 
 
-subtractButton.addEventListener('click', function(){
-    currentOperation ="-"
-    console.log (currentOperation);
+form.addEventListener('submit', function(e){
+  e.preventDefault();
 
-});
-
-multiplyButton.addEventListener('click', function(){
-    currentOperation = '×'
-     console.log (currentOperation);
-
-});
-
-divideButton.addEventListener('click', function(){
-    currentOperation = '÷'
-     console.log (currentOperation);
-
-});
+  const fullNameValue = fullName.value;
 
 
-equalsButton.addEventListener('click', function(){
- const value1 = Number(num1.value);
- const value2 = Number(num2.value);
+  if (fullNameValue === '') {
 
+    alert('Enter fullname');
 
- if (currentOperation === "+"){
-
-   result = (value1+value2)
-   console.log(result );
-}
-
-else if(currentOperation ==='-'){
-
-   result =(value1-value2);
-console.log(result);
-}
-
-
-else if(currentOperation === '×'){
-
-    result=(value1*value2);
-     console.log(result);
-}
-
-else if(currentOperation === '÷'){
-
-    result=(value1/value2);
-    console.log(result)
-}
-
-document.getElementById('result').innerHTML=result;
-
-});
-
-// const operatorButtons = document.querySelectorAll('.buttons button');
-
-// operatorButtons.forEach(button => {
-//     button.addEventListener('click', (event) => {
-//           currentOperation = event.target.innerText;
-//           console.log("Current operation set to:", currentOperation);
-//     });
-
-
-// });
-
-
-
-clearButton.addEventListener('click', function(){
-
-num1.value='';
-num2.value='';
-results.innerText = '';
-currentOperation = 'Results';
-
-})
-
-// class car {
-//     constructor( brand, model, year){
-
-//         this.brand=brand;
-//          this.model=model;
-//           this.year=year;
-
-//     }
-//     getInfo(){
-//     console.log (`This is a ${this.year} ${this.brand} ${this.year}`);
-
-// }
-// startEngine(){
-//     console.log(`Vromm ${this.brand} is starting`);
     
-// }
-
-
-// }
-
-// const car1= new car("Volkswagen", "golfR", 2022)
-// const car2= new car("Toyota", "caldina", 2025)
-
-
-// car1.getInfo();
-// car1.startEngine();
-// car2.getInfo();
-
-
-
-
-
-
+  }
+})
