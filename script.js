9fac33cdb616d034f6ff49c912a3549818b4b57a
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-07 12:40:46 +0300

    Form validation conditions

--- script.js
+++ script.js
@@ -11,12 +11,46 @@ form.addEventListener('submit', function(e){
   e.preventDefault();
 
   const fullNameValue = fullName.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const confirmPasswordValue = confirmPassword.value;
+  const checkBoxValue = checkBox;
 
 
   if (fullNameValue === '') {
 
     alert('Enter fullname');
+    return false;
 
     
   }
+  else if(emailValue ===''){
+
+  
+alert('Enter Email')
+  }
+
+  else if(passwordValue===''){
+    alert('Passowrd Cannot be empty');
+    return false;
+
+  }
+  else if (confirmPasswordValue === ''){
+    alert('Confirm Password');
+    return false;
+  }
+
+  else if(passwordValue !== confirmPasswordValue){
+  
+    alert("Password Dont Match");
+
+  }
+  else if(!checkBoxValue.checked){
+
+    alert('You must agree to terms')
+  }
+  else{
+    alert('Form submitted succesfully');
+  }
+
 })
