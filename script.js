e3dab7865556755d4f6545e6a810b25a221987eb
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-08 13:05:21 +0300

    Dislayed errors on design page

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ const confirmPassword= document.getElementById('confirmPassword');
 const checkBox = document.getElementById('terms');
 const btn = document.getElementById('createAccount');
 
+// error mesage
+
+const fullNameError = document.getElementById('fullname-error');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+const confirmPasswordError = document.getElementById('confirmPassword-error');
+
+
+function showError(input, errorElement, mesage){
+ errorElement.textContent = mesage;
+ input.style.border='2px solid red';
+
+}
+
+function clearError(input, errorElement){
+  errorElement.textContent='';
+  input.style.border='';
+}
 
 form.addEventListener('submit', function(e){
   e.preventDefault();
@@ -19,38 +37,48 @@ form.addEventListener('submit', function(e){
 
   if (fullNameValue === '') {
 
-    alert('Enter fullname');
-    return false;
-
+     showError(fullName, fullNameError, 'Enter Full Name');
+    
     
   }
   else if(emailValue ===''){
 
   
-alert('Enter Email')
+showError(email, emailError, 'Enter Email');
   }
 
   else if(passwordValue===''){
-    alert('Passowrd Cannot be empty');
-    return false;
+   showError(password, passwordError, 'Enter Password');
+   
 
   }
   else if (confirmPasswordValue === ''){
-    alert('Confirm Password');
-    return false;
+    showError(confirmPassword, passwordError, 'Confirm Password');;
+  
   }
+  
+  else if(confirmPasswordValue.length<5){
+   showError(fullName, fullNameError, 'Password Must contain 5 Characters');
+  }
+  
 
   else if(passwordValue !== confirmPasswordValue){
   
-    alert("Password Dont Match");
+    showError(fullName, fullNameError, 'Password dont Match');;
 
   }
+
   else if(!checkBoxValue.checked){
 
     alert('You must agree to terms')
   }
+
   else{
-    alert('Form submitted succesfully');
+    alert('Sign Up Successfully');
+  
   }
+ 
 
 })
+
+
