d640713f1f1d3ef3703338c54a94e59b1c16561e
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-15 16:03:22 +0300

    toggles button ins js

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,7 @@
 const logoutBtn = document.getElementById('logoutBtn');
 const getMealBtn = document.getElementById('getMealBtn');
 const loading = document.getElementById('loading');
+const mealName = document.getElementById('mealName');
 const mealDisplay = document.getElementById('mealDisplay');
 const mealCategory =document.getElementById('mealCategory');
 const mealArea = document.getElementById('mealArea');
@@ -21,8 +22,21 @@ getMealBtn.addEventListener('click', function(){
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
  .then(Response =>Response.json())
  .then(data =>{
+console.log(data.meals[0].strMeal);
+console.log(data.meals[0].strCategory);
+console.log(data.meals[0].strArea);
 console.log(data.meals[0]);
 
+
+getMealBtn.addEventListener('click', function () {
+
+  mealDisplay.classList.toggle('active');
+
+});
+
+toggleClass();
+
+mealName.innerHTML=(data.meals[0].strMeal);
  })
 
 })
