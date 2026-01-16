21eba74aaee1b57d44e7a9c62fea07cf6fa70ed0
rojimkolwey <rodgersmkolwe@gmail.com>
2026-01-16 14:29:47 +0300

    displayed meals

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,7 @@ const mealName = document.getElementById('mealName');
 const mealDisplay = document.getElementById('mealDisplay');
 const mealCategory =document.getElementById('mealCategory');
 const mealArea = document.getElementById('mealArea');
+const mealImage = document.getElementById('mealImage')
 
 
 // //log out
@@ -19,24 +20,30 @@ logoutBtn.addEventListener('click', function(){
 
 getMealBtn.addEventListener('click', function(){
 
+  loading.style.display='block'
+
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
  .then(Response =>Response.json())
  .then(data =>{
+  const meal = data.meals[0];
 console.log(data.meals[0].strMeal);
 console.log(data.meals[0].strCategory);
 console.log(data.meals[0].strArea);
 console.log(data.meals[0]);
 
+mealName.innerHTML = meal.strMeal;
+mealCategory.innerHTML = meal.strCategory;
+mealArea.innerHTML = meal.strArea;
+mealImage.src = meal.strMealThumb;
 
-getMealBtn.addEventListener('click', function () {
 
-  mealDisplay.classList.toggle('active');
 
-});
+//show meal
+ mealDisplay.classList.add('active');
 
-toggleClass();
 
-mealName.innerHTML=(data.meals[0].strMeal);
+  // Hide loading
+      loading.style.display = 'none';
  })
 
 })
